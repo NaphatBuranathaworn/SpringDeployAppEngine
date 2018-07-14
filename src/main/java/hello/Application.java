5c8453636968282c5054bd187e54206ad7b42316
NaphatBuranathaworn <naphat.bur@gmail.com>
2018-07-14 15:48:43 +0700

    dddd

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,16 +1,23 @@
 package hello;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import controller.HelloWorldController;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = HelloWorldController.class)
 public class Application {
-	
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello Spring Boot!";
+	}
 
 }
